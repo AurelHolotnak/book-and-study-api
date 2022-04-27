0546ab6a4a3bedabc1c4d91ddd8ed598b952bc31
NGDALE <marek.motuz8@gmail.com>
2022-04-27 22:21:22 +0200

    updating api

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,20 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import Clerk from '@clerk/clerk-sdk-node/dist/Clerk';
 import { PrismaClient } from '@prisma/client';
+import { connect } from "mqtt"  // import connect from mqtt
 
 import labRouter from './routes/lab-router';
 import userRouter from './routes/user-router';
+import mqttRouter from "./routes/mqtt-router";
+
 
 dotenv.config({ path: '.env' });
 
 export const clerk = new Clerk({ apiKey: process.env.CLERK_API_KEY });
 export const prisma = new PrismaClient();
+
+// export const mqtt  = connect('broker.hivemq.com')
+
 prisma.$connect();
 
 const app = express();
@@ -25,7 +31,10 @@ app.use(express.json());
 app.use(clerk.expressWithAuth());
 app.use(labRouter);
 app.use(userRouter);
+app.use(mqttRouter);
 
 app.listen(4001, () => {
   console.log('Server started on port 4000');
 });
+
+
