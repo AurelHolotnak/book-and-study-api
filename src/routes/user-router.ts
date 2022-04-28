2a42576eea342dfa50883a6050f839fe3d92cbc7
Marco Mrúz <marcomruz1@gmail.com>
2022-04-28 19:02:04 +0200

    remove verification of svix

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -13,17 +13,8 @@ router.post(
   bodyParser.raw({ type: 'application/json' }),
   // @ts-ignore - express-clerk doesn't have a type for this
   async (req: WithAuthProp<Request>, res) => {
-    const payload = req.body;
-    const headers: any = req.headers;
-
-    const webhook = new Webhook(process.env.SVIX_SECRET!);
-
     try {
-      const { data }: { data: ClerkUser } = webhook.verify(
-        payload,
-        headers
-      ) as { data: ClerkUser };
-
+      const { data } = req.body;
       const user = await prisma.user.findUnique({
         where: {
           email: data.email_addresses[0].email_address,
